f6f9c1d3babbaf3292602eb0943d70f6305c37b7
顾霖轩 <2495802072@qq.com>
2024-06-29 20:34:05 +0800

    初步加入玩家

--- script/PlayerControl.gd
+++ script/PlayerControl.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d:AnimatedSprite2D = $AnimatedSprite2D as AnimatedSprite2D
+
+
 var speed:int = 100
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") # 重力
 
@@ -8,10 +11,12 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") # 重力
 func _physics_process(delta):
 	if gravity_open:
 		if not is_on_floor():
+			animated_sprite_2d.set_animation("Fall")
 			velocity.y += gravity * delta
 		if Input.is_action_just_pressed("jump") and is_on_floor():
-			velocity.y = 400
-		
+			animated_sprite_2d.set_animation("default")
+			velocity.y = -400
+	
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * speed
