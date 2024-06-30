52e838eb9110d12ecdc41ad57ae957b34edb78d9
顾霖轩 <2495802072@qq.com>
2024-06-30 18:25:05 +0800

    GUI界面初构

--- Global/script/Global.gd
+++ Global/script/Global.gd
@@ -1,3 +1,4 @@
+## 游戏全局设置，自动加载中命名为G
 extends Node
 
 signal game_entered() ##完全进入游戏后触发
@@ -6,8 +7,6 @@ signal game_exited() ##从游戏中退出触发
 var override_file:String 
 var language_file:String 
 var multi_file:String
-var player_local_dir_path:String = ProjectSettings.globalize_path("user://Data/player/")
-var world_local_dir_path:String = ProjectSettings.globalize_path("user://Data/world/")
 
 var PLAY_MODE := PLAY_MODES.SINGLEPLAYER ##单人游戏或者多人游戏
 enum PLAY_MODES{
@@ -28,7 +27,7 @@ func _init(): ## 初始化设置文件，语言文件，多人设置文件s
 		language_file = OS.get_executable_path().get_base_dir().path_join("language.json")
 		multi_file = OS.get_executable_path().get_base_dir().path_join("multiSetting.cfg")
 
-func _ready():
+func _ready(): ## 获取语言文件中的地区
 	if FileAccess.file_exists(language_file):
 		var strs = FileAccess.get_file_as_string(language_file)
 		var json = JSON.new()
@@ -37,41 +36,34 @@ func _ready():
 			var data = json.data
 			if typeof(data) == TYPE_DICTIONARY and data.has("language"):
 				var lang = data["language"]
-				_change_language(lang)
+				_change_language(lang) #设置游戏语言
 			else:
 				print("Unexpected data")
 		else:
 			print("JSON Parse Error: ", json.get_error_message(), " in ", strs, " at line ", json.get_error_line())
 		
-	else:
-		pass
+	pass
 
-func _get_game_root() -> Node:
+func _get_game_root() -> Node: ## 获取游戏根节点
 	return get_node("/root/Game")
 
-func _get_player_local_dir_path() -> String:
-	return player_local_dir_path
-
-func _get_world_local_dir_path() -> String:
-	return world_local_dir_path
-
-func _get_view_manager() -> GUIViewManager:
-	var manager:GUIViewManager =  _get_game_root().get_node_or_null("%GUIViewManager")
+func _get_view_manager() -> GUIManager: ## 获得GUI界面管理器
+	var manager:GUIManager =  _get_game_root().get_node_or_null("%GUIManager")
 	if manager:
 		return manager
 	else:
 		print("can not find manager")
 		return
 
-func _save_game_settings():
+func _save_game_settings(): ## 保存游戏设置
 	ProjectSettings.save_custom(override_file)
 	pass
 
-func _change_language(lang:String):
+func _change_language(lang:String):## 修改游戏地区（语言）
 	if TranslationServer.get_locale() != lang:
 		TranslationServer.set_locale(lang)
 
-func _save_language(lang:String):
+func _save_language(lang:String): ## 保存当前游戏地区（语言）
 	var data = {
 		"language":lang
 	}
@@ -80,7 +72,7 @@ func _save_language(lang:String):
 	file.store_string(strs)
 	pass
 
-func _get_palyer_camera() -> Camera2D:
+func _get_palyer_camera() -> Camera2D: ## 获取玩家相机
 	var player_camera:Camera2D = _get_game_root().get_node_or_null("%PlayerCamera")
 	if player_camera:
 		return player_camera
@@ -88,10 +80,10 @@ func _get_palyer_camera() -> Camera2D:
 		print("can not find camera")
 		return
 
-func has_multi_file() -> bool:
+func has_multi_file() -> bool: ## 返回是否存在多人游戏文件
 	return FileAccess.file_exists(multi_file)
 
-func get_multi_file_path() -> String:
+func get_multi_file_path() -> String:## 返回多人游戏文件
 	return multi_file
 
 func host_game() -> void: ##主机创建游戏
@@ -158,28 +150,12 @@ func join_game() -> void: ##服务端加入游戏
 		printerr("多人文件丢失")
 		return
 
-func get_play_mode():
+func get_play_mode(): ## 获取游戏模式
 	return PLAY_MODE
 
-func _get_role_manager() -> RoleManager:
-	var manager:RoleManager =  _get_game_root().get_node_or_null("%RoleManager")
-	if manager :
-		return manager
-	else:
-		print("can not find manager")
-		return
-
-func _get_world_manager() -> WorldManager:
-	var manager:WorldManager =  _get_game_root().get_node_or_null("%WorldManager")
-	if manager :
-		return manager
-	else:
-		print("can not find manager")
-		return
-
-func _get_BGM_Player() -> AudioStreamPlayer:
+func _get_BGM_Player() -> AudioStreamPlayer: ## 获取BGM音频节点
 	return $BGMAudioStreamPlayer as AudioStreamPlayer
 
-func _get_SFX_Player() -> AudioStreamPlayer:
+func _get_SFX_Player() -> AudioStreamPlayer:## 获取SFX音频节点
 	return $SFXAudioStreamPlayer as AudioStreamPlayer
 
---
+++ UI/script/Code/BaseGui.gd
@@ -0,0 +1,52 @@
+## GUI的基类
+## 包含GUI的主要操作以及功能的预编译
+class_name BaseGUI extends Control
+
+signal be_opened
+signal be_closed
+signal be_hided
+signal be_showed
+var config:GUIViewConfig
+var viewInstanceID:int = -1
+
+
+func open(): ## 打开该界面
+	be_opened.emit()
+	_open()
+
+func close():## 关闭该界面
+	be_closed.emit()
+	_close()
+
+func be_hide():## 该界面被隐藏
+	be_hided.emit()
+	_be_hide()
+
+func be_show():## 该界面被显示
+	be_showed.emit()
+	_be_show()
+
+func _open():## 打开该界面时触发的方法
+	pass
+
+func _close():## 关闭该界面时触发的方法
+	pass
+
+func _be_hide():## 该界面被隐藏时触发的方法
+	pass
+
+func _be_show():## 该界面被显示时触发的方法
+	pass
+
+func _close_self():## 当前界面面关闭自己
+	G._get_view_manager().close_view(viewInstanceID)
+
+func _hide_others():## 隐藏该界面外其他界面
+	for InstanceId:int in G._get_view_manager().viewInstanceMap:
+		if InstanceId != viewInstanceID:
+			G._get_view_manager().hide_view(InstanceId)
+
+func _show_others():## 显示该界面外其他界面
+	for InstanceId:int in G._get_view_manager().viewInstanceMap:
+		if InstanceId != viewInstanceID:
+			G._get_view_manager().show_view(InstanceId)
