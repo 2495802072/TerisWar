a5ad4559c63fa4df540ec325c1db1a90b521fdc7
顾霖轩 <2495802072@qq.com>
2024-07-01 16:41:48 +0800

    增加翻译

--- Map/script/Blocks.gd
+++ Map/script/Blocks.gd
@@ -10,7 +10,6 @@ func _ready():
 
 func thread_generate()->void: ##线程生成地图
 	randomize()
-	print(cells_dic.size())
 	var i = randi_range(0,cells_dic.size()-1)
 	set_cells_terrain_connect(0,cells_dic[i].cells_array,0,0,true)
 	pass
