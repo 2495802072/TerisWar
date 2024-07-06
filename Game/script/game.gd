3698440d6438ab8289f8043817c5d456dcd23f81
顾霖轩 <2495802072@qq.com>
2024-07-06 16:23:42 +0800

    界面切换，及游戏初始化

--- Game/script/game.gd
+++ Game/script/game.gd
@@ -1,3 +1,16 @@
 extends Node
 
+@export var map_root:Node2D
+@export var player_root:Node2D
 var map_scene:PackedScene = preload("res://Map/scene/map.tscn") ## 基础地图
+var player_scene:PackedScene = preload("res://NPC & Player/scene/player.tscn") ## 基础地图
+
+func refresh_map(): ## 刷新地图
+	for node in map_root.get_children():
+		node.free()
+	map_root.add_child(map_scene.instantiate())
+	pass
+
+func add_player():## 添加玩家
+	player_root.add_child(player_scene.instantiate())
+	pass
